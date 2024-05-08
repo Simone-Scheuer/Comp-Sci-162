62998c009b64a2c9ebcbca0b8df7a10cc6bf512e
Simone Scheuer <simones@ada.cs.pdx.edu>
2024-05-08 14:41:02 -0700

    commiting to be safe

--- prog3/header.h
+++ prog3/header.h
@@ -2,21 +2,31 @@
 // Author: Simone Scheuer
 // Date: May 4, 2024
 // Description: Header file.
-
+#include <fstream>
 #include <iostream>
 #include <cstring>
 #include <cctype>
 using namespace std;
 
-const int SIZE{100};
+const int SEARCH{31};
+const int NAME{31};
+const int FRANCHISE{31};
+const int DESCRIPTION{100};
+const int COLOR{31};
 
 struct Item
 {
-    char name[SIZE];
-    char franchise[SIZE];
-    char description[SIZE];
-    char color[SIZE];
+    char name[NAME];
+    char franchise[FRANCHISE];
+    char description[DESCRIPTION];
+    char color[COLOR];
     int quantity;
     bool sale;
     double price;
 };
+
+void print_inventory(Item inventory[], int& num_of_items);
+void menu(Item inventory[], int& num_of_items);
+void clear_screen();
+void create_item(Item& an_item);                                                         
+void search_inventory(Item inventory[], int& num_of_items);
--- prog3/main.cpp
+++ prog3/main.cpp
@@ -1,26 +1,158 @@
 #include "header.h"
+void print_item(Item inventory[], int& i);
+void print_welcome();
+void search_list();
+void clear_screen();
+
 
 int main() {
-    Item myItem;  // Creating an instance of Item
-
-    // Assigning values to the struct members
-    strcpy(myItem.name, "Superhero Figurine");
-    strcpy(myItem.franchise, "Marvel");
-    strcpy(myItem.description, "A detailed figurine of Spider-Man.");
-    strcpy(myItem.color, "Red and Blue");
-    myItem.quantity = 50;
-    myItem.sale = false;
-    myItem.price = 29.99;
-
-    // Output some information about the item
-    cout << "Item: " << myItem.name << endl;
-    cout << "Franchise: " << myItem.franchise << endl;
-    cout << "Description: " << myItem.description << endl;
-    cout << "Color: " << myItem.color << endl;
-    cout << "Quantity: " << myItem.quantity << endl;
-    cout << "On Sale: " << (myItem.sale ? "Yes" : "No") << endl;
-    cout << "Price: $" << myItem.price << endl;
+    Item inventory[20];  // Creating an instance of Item
+    int num_of_items{0};
+    print_welcome();
+    menu(inventory, num_of_items);
 
     return 0;
 }
 
+
+void print_welcome()
+{
+    clear_screen();
+    cout << "\n Welcome to Mona's Inventory Management Program \n";
+    cout << "---------------------------------------------------\n";
+}
+
+
+void menu (Item inventory[], int& num_of_items)
+{
+    int choice{0};
+    do
+    {
+        cout << "\n 1. Register a new item. \n 2. Print current inventory."
+            << "\n 3. Search current inventory. \n 4. Save and quit. \n\n";  
+
+        cout << " Enter your choice: ";
+        if (!(cin >> choice))
+        {
+            cin.clear();
+            cin.ignore(100, '\n');
+            cout << " Invalid choice. Please enter 1-4. \n";
+            continue;
+        }
+        cin.ignore(100, '\n');
+
+        if (choice < 1 || choice > 4)
+        {
+            clear_screen();
+            cout << " Invalid choice. Please enter 1-4. \n";
+        }
+        else if (choice == 1)
+        {
+            clear_screen();
+            create_item(inventory[num_of_items]);
+            clear_screen();
+            num_of_items++;
+        }
+        else if (choice == 2)
+        {
+            clear_screen();
+            print_inventory(inventory, num_of_items);
+        }
+        else if (choice == 3)
+        {
+            search_inventory(inventory, num_of_items);
+        }
+    } while (choice != 4); 
+}
+
+
+void clear_screen()
+{
+    cout << "\033[2J\033[H"; 
+}
+
+
+void create_item(Item& an_item)
+{
+    char sale_status_indicator{'z'};
+
+    cout << "\n Please enter item name: ";
+    cin.get(an_item.name, NAME, '\n');
+    cin.ignore(100, '\n');
+
+    cout << "\n Please enter item franchise: ";
+    cin.get(an_item.franchise, FRANCHISE, '\n');
+    cin.ignore(100, '\n'); 
+
+    cout << "\n Please enter item description: ";
+    cin.get(an_item.description, DESCRIPTION, '\n');
+    cin.ignore(100, '\n'); 
+
+    cout << "\n Please Enter item color(s): ";
+    cin.get(an_item.color, COLOR, '\n');
+    cin.ignore(100, '\n');
+
+    cout << "\n Please enter item quantity: ";
+    cin >> an_item.quantity;
+    cin.ignore(100, '\n');
+
+    cout << "\n Please enter item price: ";
+    cin >> an_item.price;
+    cin.ignore(100, '\n');
+
+    cout << "\n Please enter item sale status (y/n): ";
+    cin >> sale_status_indicator;
+    cin.ignore(100, '\n');
+}
+
+
+void print_inventory(Item inventory[], int& num_of_items)
+{
+    if (num_of_items == 0)
+    {
+        cout << "\n Zero items currently registered. \n";
+    }
+    cout << "-------------------------------------------------";                           
+    for (int i{0}; i < num_of_items; i++)
+    {
+        print_item(inventory, i);
+    }
+}
+
+
+void search_inventory(Item inventory[], int& num_of_items)
+{
+    char search_term[SEARCH];
+    cout << "\nEnter the item name to search: ";
+    cin.get(search_term, SEARCH, '\n');
+    cin.ignore(100, '\n'); 
+
+    bool found = false;
+    for (int i{0}; i < num_of_items; i++)
+    {
+        if (strcmp(search_term, inventory[i].name) == 0)
+        {
+            print_item(inventory, i);
+            found = true;
+
+        }
+    }
+
+    if (!found)
+    {
+        cout << "No items found with the name '" << search_term << "'.\n";
+    }
+}
+
+void print_item(Item inventory[], int& i)
+{
+    cout << "\n Item " << (i + 1) << ":"
+        << "\n Name: " << inventory[i].name
+        << "\n Franchise: " << inventory[i].franchise
+        << "\n Description: " << inventory[i].description
+        << "\n Color: " << inventory[i].color
+        << "\n Quantity: " << inventory[i].quantity
+        << "\n Price: $" << inventory[i].price
+        << "\n Sale: " << (inventory[i].sale ? "Yes" : "No") << "\n"
+        << "-------------------------------------------------\n";
+}
